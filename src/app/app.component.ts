7fcbde5618bfae2fed6987315e9bd33bb1e50c0a
Arkesh Kalathiya <arkesh486855@gmail.com>
2020-08-02 09:05:34 +0530

    Added demo page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,8 +18,8 @@ export class AppComponent implements OnInit {
       icon: 'mail'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
+      title: 'Demo',
+      url: '/demo',
       icon: 'paper-plane'
     },
     {
